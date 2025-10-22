cfb186305403afc6d8dcb9fe79d2ab3c77e5f5b1
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-10-22 16:17:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Setup
+
+### 1. Setup Qdrant using Docker
+```
+docker compose up -d
+```
+
+### 2. Install the Python dependencies
+```
+pip install requirments.txt
+```
+
+### 3. Run the Indexing Program
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+pdf_path = Path(__file__).parent / "pondi.pdf"
+loader = PyPDFLoader(pdf_path)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400,
+)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+chunks = text_splitter.split_documents(documents=docs)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    url="http://localhost:6333",
+    collection_name="NIRF Information",
+    embedding=embeddings,
+)
+
