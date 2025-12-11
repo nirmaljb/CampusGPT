e7e92aee1f2e98530f113196da82529a56b372c1
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-12-11 10:34:02 +0530

    made some changes to the prompt: added user context history

--- backend/queues/workers.py
+++ backend/queues/workers.py
@@ -4,6 +4,10 @@ load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from langgraph.graph import MessagesState, StateGraph, START, END
+
+class State(MessagesState):
+    messages: list[str]
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -11,7 +15,6 @@ model = ChatGoogleGenerativeAI(
 )
 
 model_with_search = model.bind_tools([{"google_search": {}}])
-
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
 vector_db = QdrantVectorStore.from_existing_collection(
@@ -20,13 +23,18 @@ vector_db = QdrantVectorStore.from_existing_collection(
     embedding=embeddings
 )
 
-def process_query(query: str):    
-    search_result = vector_db.similarity_search(query=query)
+def process_query(state: MessagesState):    
+    search_result = vector_db.similarity_search(query=state['messages'][-1])
     context = "\n\n".join([f"{result.page_content}\n" for result in search_result])
+
+    print(state)
 
     SYSTEM_PROMPT = f"""
         You're an expert consultant for Pondicherry University management and students.
 
+        **USER CONVERSATION HISTORY:**
+        {state['messages']}
+        
         **Instructions:**
         1. First, analyze the INTERNAL DATABASE CONTEXT below
         2. Use Google Search to find current information and verify/supplement the internal data
@@ -42,10 +50,24 @@ def process_query(query: str):
         Now answer the user's query using both the internal context and web search results.
     """
 
-    messages = [
+    query = [
         ("system",SYSTEM_PROMPT),
-        ("human", query),
+        ("human", state['messages'][-1]),
     ]
 
-    llm_response = model_with_search.invoke(messages)
-    return llm_response.content
+    llm_response = model_with_search.invoke(query)
+    return { "messages": llm_response.content }
+
+builder = StateGraph(State)
+
+builder.add_node("process_query", process_query)
+builder.add_edge(START, "process_query")
+builder.add_edge("process_query", END)
+
+agent = builder.compile()
+
+while(True):
+    user_input = input()
+    response = agent.invoke({"messages": [user_input]})
+
+    print(response)
